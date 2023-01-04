e58f601ee59ba3901507e269204279aa03f7a4a4
Damian <dkowalski.1997@gmail.com>
2023-01-04 21:27:04 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rapid-hcm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'rapid-hcm';
+}
