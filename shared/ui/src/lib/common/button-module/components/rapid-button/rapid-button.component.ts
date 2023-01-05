059a9cf1b383f0ef242dcaea59cb7c27a9053052
Damian <dkowalski.1997@gmail.com>
2023-01-05 01:08:44 +0100

    initial commit + configuration for library

---
+++ shared/ui/src/lib/common/button-module/components/rapid-button/rapid-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'rapid-button',
+    templateUrl: './rapid-button.component.html'
+})
+export class RapidButtonComponent {
+}
