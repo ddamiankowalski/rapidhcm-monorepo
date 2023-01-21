dd91d5a31cb4288080f7031acf45228092978ee8
Damian Kowalski <dkowalski.1997@gmail.com>
2023-01-21 11:21:55 +0100

    refactoring of refresh token controller

--- api/src/controllers/auth/auth.controller.ts
+++ api/src/controllers/auth/auth.controller.ts
@@ -1,14 +1,13 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 
-interface RapidJwtPayload {
-    exp: number | false;
-    username: string | false;
+interface RapidJwtPayload extends JwtPayload {
+    exp: number | undefined;
+    username: string;
 }
 
 export const verifyRefreshToken = (refreshToken: string): RapidJwtPayload => {
     try {
-        const verifyResult = jwt.verify(JSON.parse(refreshToken), process.env.TOKEN_SECRET_REFRESH ?? '') as JwtPayload;
-        return verifyResult.exp && verifyResult ? { exp: verifyResult.exp, username: verifyResult.username } : { exp: false, username: false };
+        return jwt.verify(JSON.parse(refreshToken), process.env.TOKEN_SECRET_REFRESH ?? '') as RapidJwtPayload;
     } catch(err) {
         console.error('Could not verify token');
         throw(err);
