81651681ac03b44a8773f2600df055ff9e6f7686
Damian <dkowalski.1997@gmail.com>
2023-01-26 19:33:24 +0100

    toast logic

--- shared/ui/src/lib/global/toast-module/components/rapid-toast-outlet/rapid-toast-outlet.component.ts
+++ shared/ui/src/lib/global/toast-module/components/rapid-toast-outlet/rapid-toast-outlet.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { RapidToastService } from '../../services/rapid-toast.service';
 import { RapidToastComponent } from '../rapid-toast/rapid-toast.component';
 
@@ -16,14 +16,27 @@ export class RapidToastOutletComponent implements AfterViewInit {
     ) {}
 
     ngAfterViewInit(): void {
-        console.log(this.toastOutletContainer);
         const interval = setInterval(() => {
-            this.toastOutletContainer.createComponent(RapidToastComponent);
-            this.cdRef.markForCheck();
+            this.appendNewToast('Witam', 'nie wie co to');
         }, 1000);
 
         setTimeout(() => {
             clearInterval(interval)
         }, 5001)
+    }
+
+    public appendNewToast(title: string, subtitle: string): void {
+        const newToast = this.toastOutletContainer.createComponent(RapidToastComponent);
+        this.setToastMessage<RapidToastComponent>(title, subtitle, newToast);
+    }
+
+    private setToastMessage<T>(titleValue: string, subtitleValue: string, toastRef: ComponentRef<T>): void {
+        this.setToastInput<T, string>('title', titleValue, toastRef);
+        this.setToastInput<T, string>('subtitle', subtitleValue, toastRef);
+        this.cdRef.markForCheck();
+    }
+
+    private setToastInput<T, V = unknown>(inputName: string, inputValue: V, toastRef: ComponentRef<T>): void {
+        toastRef.setInput(inputName, inputValue);
     }
 }
--- shared/ui/src/lib/global/toast-module/components/rapid-toast/rapid-toast.component.ts
+++ shared/ui/src/lib/global/toast-module/components/rapid-toast/rapid-toast.component.ts
@@ -1,10 +1,19 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { RapidToastService } from "../../services/rapid-toast.service";
 
 @Component({
     selector: 'rapid-toast',
-    templateUrl: './rapid-toast.component.html'
+    templateUrl: './rapid-toast.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RapidToastComponent {
+    constructor(
+        private toast: RapidToastService
+    ) {}
+
+    @Input() title?: string;
+    @Input() subtitle?: string;
+
     public handleToastClick(): void {
         console.log('ehhe')
     }
