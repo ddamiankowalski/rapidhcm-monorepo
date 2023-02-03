fee6415012b81f550e4e7f249a34384109acf22d
Damian <dkowalski.1997@gmail.com>
2023-02-03 19:31:47 +0100

    added navigation

---
+++ shared/ui/src/lib/global/sidenav-module/components/rapid-sidenav-header/rapid-sidenav-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'rapid-sidenav-header',
+    templateUrl: './rapid-sidenav-header.component.html'
+})
+export class RapidSidenavHeaderComponent {
+    public title = 'Shared Calendars';
+}
--- shared/ui/src/lib/global/sidenav-module/rapid-sidenav.module.ts
+++ shared/ui/src/lib/global/sidenav-module/rapid-sidenav.module.ts
@@ -1,16 +1,20 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { RapidIconsModule } from "../../common";
+import { RapidSidenavHeaderComponent } from "./components/rapid-sidenav-header/rapid-sidenav-header.component";
 import { RapidSidenavComponent } from "./components/rapid-sidenav/rapid-sidenav.component";
 
 @NgModule({
     declarations: [
-        RapidSidenavComponent
+        RapidSidenavComponent,
+        RapidSidenavHeaderComponent
     ],
     exports: [
         RapidSidenavComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RapidIconsModule
     ]
 })
 export class RapidSidenavModule {}
