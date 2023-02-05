21fef26a70097cd38388ed40434fd8db48f2d958
Damian <dkowalski.1997@gmail.com>
2023-02-06 00:25:01 +0100

    added recemtheaderadd

--- shared/ui/src/lib/global/recent-header-module/components/rapid-header-add/rapid-recent-header-add.component.ts
+++ shared/ui/src/lib/global/recent-header-module/components/rapid-header-add/rapid-recent-header-add.component.ts
@@ -2,9 +2,11 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
     selector: 'rapid-recent-header-add',
-    templateUrl: './rapid-header-add.component.html',
+    templateUrl: './rapid-recent-header-add.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RapidRecentHeaderAddComponent {
-
+    public addWidget(): void {
+        console.log('add new widget');
+    }
 }
--- shared/ui/src/lib/global/recent-header-module/recent-header.module.ts
+++ shared/ui/src/lib/global/recent-header-module/recent-header.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { RapidRecentHeaderItemComponent } from "./components/rapid-recent-header-item/rapid-recent-header-item.component";
-import { RapidRecentHeaderAddComponent } from "./components/rapid-header-add/rapid-header-add.component";
+import { RapidRecentHeaderAddComponent } from "./components/rapid-header-add/rapid-recent-header-add.component";
+import { RapidButtonsModule, RapidIconsModule } from "../../common";
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        RapidButtonsModule,
+        RapidIconsModule
     ],
     exports: [
         RapidRecentHeaderItemComponent,
